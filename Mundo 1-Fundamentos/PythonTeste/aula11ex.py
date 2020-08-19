a520268aa4595be656e22da378384ab5bb91733f
Jose Paulo Gabriel Dos Santos <40630290+JotaPeGabriel@users.noreply.github.com>
2020-08-19 08:35:48 -0300

    atualização de ativadade antiga, base de dados e inicio do mundo 2 em Python

--- Mundo 1-Fundamentos/PythonTeste/aula11ex.py
+++ Mundo 1-Fundamentos/PythonTeste/aula11ex.py
@@ -1,1 +1,8 @@
-print('\033[1;31;43mOlá, Mundo!')
+nome = 'JPGabriel'
+cores = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo': '\033[33m',
+         'pretoebranco':'\033[7;30m'}
+print('Ola! Muito prazer em te conhecer, {}{}{}!!'.format(cores['azul'], nome, cores['limpa']))
+
+#print('\033[1;31;43mOlá, Mundo!')
