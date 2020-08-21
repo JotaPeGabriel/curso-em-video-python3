439ee2804e97f9191500811d19ce2ad03caa8801
Jose Paulo Gabriel Dos Santos <40630290+JotaPeGabriel@users.noreply.github.com>
2020-08-21 20:29:55 -0300

    Exercicio 45 - jokenpo

---
+++ Mundo 2-EstruturasDeControle/Correções/ex045.py
@@ -0,0 +1,44 @@
+"""Exercício Python 045: Crie um programa que faça o computador jogar Jokenpô com você."""
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+jogador = int(input('Qual é a sua jogada? '))
+print('JO'), sleep(1)
+print('KEN'), sleep(1)
+print('PO!!!!!')
+print('-=' * 12)
+print(f'Computador jogou {itens[computador]}')
+print(f'Jogador jogou {itens[jogador]}')
+print('-=' * 12)
+if computador == 0:  # Computador jogou PEDRA
+    if jogador == 0:
+        print('EMPATE')
+    if jogador == 1:
+        print('JOGADOR VENCE')
+    if jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVALIDA')
+elif computador == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    if jogador == 1:
+        print('EMPATE')
+    if jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADA INVALIDA')
+elif computador == 2:  # Computador jogou TESOURA
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    if jogador == 1:
+        print('COMPUTADOR VENCE')
+    if jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVALIDA')
